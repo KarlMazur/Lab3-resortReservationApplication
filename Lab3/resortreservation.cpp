943c95c2cdf1843eaea6bb02d4fecf6b840890f9
karlMazur <mazurk@spu.edu>
2019-01-21 22:54:05 -0800

    Final Test

--- Lab3/resortreservation.cpp
+++ Lab3/resortreservation.cpp
@@ -1,11 +1,12 @@
 #include "resortreservation.h"
 #include "ui_resortreservation.h"
-
+#include "iostream"
 ResortReservation::ResortReservation(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::ResortReservation)
 {
     ui->setupUi(this);
+    std::cout<<"helloWorld"<<std::endl;
 }
 
 ResortReservation::~ResortReservation()
