ba10777bb1b44550b0a862e13a424f04043f7554
KarlMazur <mazurk@spu.edu>
2019-01-22 18:54:15 -0800

    Got the combo box to work and And update a label showing the right price

--- Lab3/resortreservation.cpp
+++ Lab3/resortreservation.cpp
@@ -1,15 +1,52 @@
 #include "resortreservation.h"
 #include "ui_resortreservation.h"
 #include "iostream"
+#include "string"
 ResortReservation::ResortReservation(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::ResortReservation)
 {
     ui->setupUi(this);
-    std::cout<<"helloWorld"<<std::endl;
+
+ui->nextButton->hide();
 }
 
 ResortReservation::~ResortReservation()
 {
     delete ui;
+}
+
+void ResortReservation::on_NextButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+
+void ResortReservation::on_radioButton_clicked()
+{
+    ui->nextButton->show();
+}
+
+void ResortReservation::on_comboBox_activated(const QString &arg1)
+{
+        QString standardTwoQueenRoom  ="A Standard 2-queen room";
+      QString standardOneKingRoom ="A Standard 1-king room";
+      QString atriumTwoQueenRoom ="An Atrium 2-queen room";
+  QString atriumOneKingRoom  ="An Atrium 1-king room";
+
+  if(arg1==standardTwoQueenRoom){
+      ui->ActualPriceLabel->setText("$284");
+  }
+  else if(arg1==standardOneKingRoom){
+      ui->ActualPriceLabel->setText("$290");
+  }
+  else if(arg1==atriumTwoQueenRoom){
+        ui->ActualPriceLabel->setText("$325");
+    }
+
+    else if(arg1==atriumOneKingRoom){
+
+        ui->ActualPriceLabel->setText("$350");
+}
 }
--- Lab3/resortreservation.h
+++ Lab3/resortreservation.h
@@ -15,6 +15,15 @@ public:
     explicit ResortReservation(QWidget *parent = nullptr);
     ~ResortReservation();
 
+private slots:
+    void on_NextButton_clicked();
+
+
+
+    void on_radioButton_clicked();
+
+    void on_comboBox_activated(const QString &arg1);
+
 private:
     Ui::ResortReservation *ui;
 };
